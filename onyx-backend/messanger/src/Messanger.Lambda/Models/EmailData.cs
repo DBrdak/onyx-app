c369f2e77fa9d7ff52259be1576420d7f9784a0b
DBrdak <brdak02dominik@gmail.com>
2024-08-17 16:21:08 +0200

    Resend fix

--- onyx-backend/messanger/src/Messanger.Lambda/Models/EmailData.cs
+++ onyx-backend/messanger/src/Messanger.Lambda/Models/EmailData.cs
@@ -1,27 +1,41 @@
-﻿using Amazon.Lambda.SNSEvents;
-using Newtonsoft.Json;
+﻿using System.Text.Json.Serialization;
+using Amazon.Lambda.SNSEvents;
 
 namespace Messanger.Lambda.Models;
 
 public sealed record EmailData
 {
-    [JsonProperty("from")]
-    public const string From = "Onyx <notify@onyxapp.tech>";
-    private const string toMapName = "Recipient";
-    [JsonProperty("to")]
+    private const string baseSender = "Onyx <notify@onyxapp.tech>";
+
+    [JsonPropertyName("from")]
+    public string From { get; init; }
+    [JsonPropertyName("to")]
     public string[] To { get; init; }
-    private const string subjectMapName = "Subject";
-    [JsonProperty("subject")]
+    [JsonPropertyName("subject")]
     public string Subject { get; init; }
-    private const string htmlMapName = "HtmlBody";
-    [JsonProperty("html")]
+    [JsonPropertyName("html")]
     public string Html { get; init; }
-    private const string plainTextBodyMapName = "PlainTextBody";
-    [JsonProperty("plainTextBody")]
+    [JsonIgnore]
     public string PlainTextBody { get; init; }
+
+    private const string toMapName = "Recipient";
+    private const string subjectMapName = "Subject";
+    private const string htmlMapName = "HtmlBody";
+    private const string plainTextBodyMapName = "PlainTextBody";
 
     private EmailData(string[] to, string subject, string html, string plainTextBody)
     {
+        From = baseSender;
+        To = to;
+        Subject = subject;
+        Html = html;
+        PlainTextBody = plainTextBody;
+    }
+
+    [JsonConstructor]
+    private EmailData(string from, string[] to, string subject, string html, string plainTextBody)
+    {
+        From = from;
         To = to;
         Subject = subject;
         Html = html;
--- onyx-backend/messanger/src/Messanger.Lambda/Services/Emails/ResendClient/ResendClient.cs
+++ onyx-backend/messanger/src/Messanger.Lambda/Services/Emails/ResendClient/ResendClient.cs
@@ -1,9 +1,11 @@
 ﻿using System.Net.Http.Json;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Messanger.Lambda.Models;
 using Models.Responses;
 using Newtonsoft.Json;
 using SharedDAL.SecretsManager;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 namespace Messanger.Lambda.Services.Emails.ResendClient;
 
@@ -21,23 +23,23 @@ internal sealed class ResendClient
         var response = await _httpClient.PostAsJsonAsync(
             sendEmailPath,
             data,
-            new JsonSerializerOptions
-            {
-                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-            },
             cancellationToken);
         
         var responseContent = await response.Content.ReadAsStringAsync(cancellationToken);
 
+        //TODO TEMP
+        var jsonData = JsonSerializer.Serialize(data);
+
         return Result.FromBool(
             response.IsSuccessStatusCode,
-            CreateResendResponseError(data.To, responseContent));
+            CreateResendResponseError(data.To, responseContent, jsonData));
     }
 
-    private static Error CreateResendResponseError(string[] recipient, object errorMessage) => new(
+    private static Error CreateResendResponseError(string[] recipient, object errorMessage, string data) => new(
         "ResendClient.EmailSendError",
         $"""
           Problem while sending an email to {JsonConvert.SerializeObject(recipient)}.
+          Request: {data}
           Response: {JsonConvert.SerializeObject(errorMessage)}.
           """);
 }
