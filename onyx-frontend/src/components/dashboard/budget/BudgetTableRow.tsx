0948d1485802a1e84f00f1c24a69cf7d15b9f375
Maciej Mądry <maciejmadry0@gmail.com>
2024-08-26 12:56:53 +0200

    ADD: new budgets table layout

--- onyx-frontend/src/components/dashboard/budget/BudgetTableRow.tsx
+++ onyx-frontend/src/components/dashboard/budget/BudgetTableRow.tsx
@@ -2,24 +2,6 @@ import { FC, useState } from "react";
 import { Link } from "@tanstack/react-router";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import { Edit, Trash } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
-
 import type { Budget } from "@/lib/validation/budget";
 import { deleteBudget, getBudgetsQueryOptions } from "@/lib/api/budget";
 import { cn } from "@/lib/utils";
@@ -28,6 +10,8 @@ import {
   DEFAULT_YEAR_STRING,
 } from "@/lib/constants/date";
 import { User } from "@/lib/validation/user";
+import { Button } from "@/components/ui/button";
+import { Ellipsis } from "lucide-react";
 
 interface BudgetTableRowProps {
   budget: Budget;
@@ -58,84 +42,41 @@ const BudgetTableRow: FC<BudgetTableRowProps> = ({ budget, user }) => {
   };
 
   return (
-    <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
-      <TooltipProvider>
-        <Tooltip delayDuration={300}>
-          <TooltipTrigger asChild>
-            <li
-              className={cn("border-t", {
-                "opacity-50": isPending || budget.optimistic,
-              })}
+    <li
+      className={cn("min-w-[600px] border-t", {
+        "opacity-50": isPending || budget.optimistic,
+      })}
+    >
+      <Link
+        to="/budget/$budgetId"
+        params={{ budgetId: budget.id }}
+        search={{
+          month: DEFAULT_MONTH_STRING,
+          year: DEFAULT_YEAR_STRING,
+          accMonth: DEFAULT_MONTH_STRING,
+          accYear: DEFAULT_YEAR_STRING,
+        }}
+        mask={{ to: `/budget/${budget.id}` }}
+        preload="intent"
+        className="grid w-full grid-cols-9 gap-x-4 px-4 py-8 hover:bg-accent"
+      >
+        <p className="col-span-3 min-w-[150px]">{budget.name}</p>
+        <p className="col-span-2 min-w-[100px]">{budget.currency}</p>
+        <div className="col-span-4 grid min-w-[250px] grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+          {budget.budgetMembers.map((member) => (
+            <p
+              key={id}
+              className="truncate rounded-full border px-2 text-center"
             >
-              <Link
-                to="/budget/$budgetId"
-                params={{ budgetId: budget.id }}
-                search={{
-                  month: DEFAULT_MONTH_STRING,
-                  year: DEFAULT_YEAR_STRING,
-                  accMonth: DEFAULT_MONTH_STRING,
-                  accYear: DEFAULT_YEAR_STRING,
-                }}
-                mask={{ to: `/budget/${budget.id}` }}
-                preload="intent"
-                className="grid w-full grid-cols-3 gap-x-4 px-4 py-8 hover:bg-accent md:grid-cols-9"
-              >
-                <p className="md:col-span-3">{budget.name}</p>
-                <p className="md:col-span-2">{budget.currency}</p>
-                <div className="grid grid-cols-1 gap-2 md:col-span-4 md:grid-cols-2 lg:grid-cols-3">
-                  {budget.budgetMembers.map((member) => (
-                    <p
-                      key={id}
-                      className="truncate rounded-full border px-2 text-center"
-                    >
-                      {member.username === user?.username
-                        ? "You"
-                        : member.username}
-                    </p>
-                  ))}
-                </div>
-              </Link>
-            </li>
-          </TooltipTrigger>
-          <TooltipContent side="left">
-            <div className="flex flex-col space-y-1">
-              <Button variant="outline" className="rounded-lg" size="icon">
-                <Edit className="size-5" />
-              </Button>
-              <DialogTrigger asChild>
-                <Button variant="outline" className="rounded-lg" size="icon">
-                  <Trash className="size-5" />
-                </Button>
-              </DialogTrigger>
-            </div>
-          </TooltipContent>
-        </Tooltip>
-      </TooltipProvider>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
-          <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
-          </DialogDescription>
-        </DialogHeader>
-        <DialogFooter className="items-center">
-          {isError && (
-            <p className="text-end text-sm text-destructive">
-              Something went wrong. Please try again.
+              {member.username === user?.username ? "You" : member.username}
             </p>
-          )}
-          <Button
-            type="submit"
-            size="sm"
-            variant="destructive"
-            onClick={onDelete}
-          >
-            Delete
-          </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          ))}
+        </div>
+      </Link>
+      <Button size="icon">
+        <Ellipsis />
+      </Button>
+    </li>
   );
 };
 
--- onyx-frontend/src/routes/_dashboard-layout/budget.index/route.lazy.tsx
+++ onyx-frontend/src/routes/_dashboard-layout/budget.index/route.lazy.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import { useSuspenseQuery } from "@tanstack/react-query";
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { createLazyFileRoute, Link } from "@tanstack/react-router";
 
-import { Minus, Plus } from "lucide-react";
-import BudgetTableRow from "@/components/dashboard/budget/BudgetTableRow";
+import { Ellipsis, Minus, Plus } from "lucide-react";
 import CreateBudgetForm from "@/components/dashboard/budget/CreateBudgetForm";
 import { Button } from "@/components/ui/button";
 
 import { getBudgetsQueryOptions } from "@/lib/api/budget";
 import { cn } from "@/lib/utils";
 import { useAuthContext } from "@/lib/hooks/useAuthContext";
+import {
+  DEFAULT_MONTH_STRING,
+  DEFAULT_YEAR_STRING,
+} from "@/lib/constants/date";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export const Route = createLazyFileRoute("/_dashboard-layout/budget/")({
   component: Budget,
@@ -38,7 +42,7 @@ function Budget() {
   };
 
   return (
-    <div className="h-full overflow-auto scrollbar-none md:pl-14 md:pt-14">
+    <div className="h-full overflow-y-auto scrollbar-thin lg:overflow-y-hidden lg:p-8">
       <header className="space-y-1 lg:space-y-2">
         <h1 className="text-3xl font-bold md:text-4xl lg:text-5xl">
           All your budgets in one place.
@@ -48,39 +52,74 @@ function Budget() {
           ones!
         </p>
       </header>
-      <section className="space-y-4 py-20">
-        <ul className="overflow-hidden rounded-lg border">
-          <li className="grid w-full grid-cols-3 gap-x-4 bg-muted p-4 text-lg font-semibold tracking-wide md:grid-cols-9">
-            <p className="md:col-span-3">Name</p>
-            <p className="md:col-span-2">Currency</p>
-            <p className="md:col-span-4">Users</p>
-          </li>
-          {budgets.map((budget) => (
-            <BudgetTableRow budget={budget} key={budget.id} user={user!} />
-          ))}
+      <section className="h-full py-10 md:pt-20">
+        <ScrollArea className="h-full w-full min-w-[650px] pb-10 pr-4">
+          <div className="sticky top-0 grid w-full grid-cols-10 gap-x-4 rounded-t-lg border-x border-t bg-muted p-4 text-lg font-semibold tracking-wide">
+            <p className="col-span-3 min-w-[150px]">Name</p>
+            <p className="col-span-2 min-w-[100px]">Currency</p>
+            <p className="col-span-5 min-w-[250px]">Members</p>
+          </div>
+          <ul className="rounded-b-lg border">
+            {budgets.map(({ id, name, currency, budgetMembers }) => (
+              <li className="grid w-full grid-cols-10 items-center text-lg">
+                <Link
+                  to={`/budget/${id}`}
+                  params={{ budgetId: id }}
+                  search={{
+                    month: DEFAULT_MONTH_STRING,
+                    year: DEFAULT_YEAR_STRING,
+                    accMonth: DEFAULT_MONTH_STRING,
+                    accYear: DEFAULT_YEAR_STRING,
+                  }}
+                  mask={{ to: `/budget/${id}` }}
+                  className="group peer col-span-9 grid w-full grid-cols-9 gap-x-4 px-4 py-8 hover:bg-accent"
+                >
+                  <p className="col-span-3 min-w-[150px]">{name}</p>
+                  <p className="col-span-2 min-w-[100px]">{currency}</p>
+                  <div className="col-span-4 grid min-w-[250px] grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+                    {budgetMembers.map((member) => (
+                      <p
+                        key={id}
+                        className="truncate rounded-full border px-2 text-center"
+                      >
+                        {member.username === user?.username
+                          ? "You"
+                          : member.username}
+                      </p>
+                    ))}
+                  </div>
+                </Link>
+                <div className="col-span-1 flex h-full min-w-[50px] items-center justify-center peer-hover:bg-accent">
+                  <Button size="icon" variant="ghost">
+                    <Ellipsis />
+                  </Button>
+                </div>
+              </li>
+            ))}
 
-          <li
-            className={cn(
-              "grid grid-rows-[0fr] overflow-hidden transition-all duration-300 ease-in-out",
-              isCreating && user && "grid-rows-[1fr] border-t",
-            )}
-          >
-            <div className="overflow-hidden">
-              <CreateBudgetForm setIsCreating={setIsCreating} user={user!} />
-            </div>
-          </li>
-        </ul>
-        <div className="flex justify-center">
-          <Button
-            variant="outline"
-            className={cn("rounded-full", isCreating && "bg-secondary")}
-            size="icon"
-            onClick={handleCreateBudgetButtonClick}
-            disabled={noBudgets}
-          >
-            {isCreating ? <Minus /> : <Plus />}
-          </Button>
-        </div>
+            <li
+              className={cn(
+                "grid grid-rows-[0fr] overflow-hidden transition-all duration-300 ease-in-out",
+                isCreating && user && "grid-rows-[1fr] border-t",
+              )}
+            >
+              <div className="overflow-hidden">
+                <CreateBudgetForm setIsCreating={setIsCreating} user={user!} />
+              </div>
+            </li>
+          </ul>
+          <div className="mt-10 flex justify-center">
+            <Button
+              variant="outline"
+              className={cn("rounded-full", isCreating && "bg-secondary")}
+              size="icon"
+              onClick={handleCreateBudgetButtonClick}
+              disabled={noBudgets}
+            >
+              {isCreating ? <Minus /> : <Plus />}
+            </Button>
+          </div>
+        </ScrollArea>
       </section>
     </div>
   );
