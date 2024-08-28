110a8768d0b4c57a53b1a3d15b0b9ca528c5b07e
DBrdak <brdak02dominik@gmail.com>
2024-08-28 23:30:48 +0200

    shared-kernel covered

--- onyx-backend/shared-kernel/tests/Models.UnitTests/Money/CurrencyTests.cs
+++ onyx-backend/shared-kernel/tests/Models.UnitTests/Money/CurrencyTests.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Models.DataTypes;
 
-namespace Models.UnitTests.Money
+namespace Models.UnitTests.Money;
+
+public sealed class CurrencyTests
 {
-    internal class CurrencyTests
+    [Fact]
+    public void FromCode_Should_Success()
     {
+        // Act
+        var currenciesCreateResult = CurrencyData.ValidCurrencies.Select(Currency.FromCode);
+        var isFailure = currenciesCreateResult.Any(result => result.IsFailure);
+
+        // Assert
+        Assert.False(isFailure);
+    }
+
+    [Fact]
+    public void FromCode_Should_Fail()
+    {
+        // Act
+        var currenciesCreateResult = CurrencyData.InvalidCurrencies.Select(Currency.FromCode);
+        var isSuccess = currenciesCreateResult.Any(result => result.IsSuccess);
+
+        // Assert
+        Assert.False(isSuccess);
     }
 }
---
+++ onyx-backend/shared-kernel/tests/Models.UnitTests/MonthDate/MonthDateData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+
+namespace Models.UnitTests.MonthDate
+{
+    public sealed class MonthDateData
+    {
+        public static readonly (int Month, int Year)[] ValidMonthsYears =
+        [
+            (1, 1),
+            (2, 2025),
+            (3, 2021),
+            (4, 2222),
+            (5, 1998),
+            (6, 123),
+            (7, 23),
+            (8, 555),
+            (9, 11111),
+            (10, 5),
+            (11, 11),
+            (12, 12)
+        ];
+
+        public static readonly (int Month, int Year)[] InvalidMonthsYears =
+        [
+            (21, -1),
+            (20, -2025),
+            (-3, -2222),
+            (-4, -22222),
+            (15, -2),
+            (0, 0),
+            (22, -1),
+            (102, -1),
+            (123, -1),
+            (13, -1)
+        ];
+
+        public static readonly DataTypes.MonthDate ExampleEarlier = DataTypes.MonthDate.Create(1, 2024).Value;
+        public static readonly DataTypes.MonthDate ExampleLater = DataTypes.MonthDate.Create(12, 2024).Value;
+        public static readonly int ExamplesMonthsPositiveInterval = 11;
+        public static readonly int ExamplesMonthsNegativeInterval = -ExamplesMonthsPositiveInterval;
+
+        public static readonly int ExampleSummand = 1;
+        public static readonly int ExampleSubtrahend = 1;
+
+        public static readonly DataTypes.MonthDate ExampleSum = DataTypes.MonthDate.Create(1, 2025).Value;
+        public static readonly DataTypes.MonthDate ExampleDiffrence = DataTypes.MonthDate.Create(11, 2024).Value;
+        public static readonly DataTypes.MonthDate ExampleLaterIncrementation = DataTypes.MonthDate.Create(
+                1,
+                2025).Value;
+        public static readonly DataTypes.MonthDate ExampleEarlierDecrementation = DataTypes.MonthDate.Create(
+            12,
+            2023).Value;
+
+        public static readonly DateTime ContainedDate = new (2024, 11, 5);
+        public static readonly DateTime NotContainedDate = new (2021, 5, 5);
+    }
+}
