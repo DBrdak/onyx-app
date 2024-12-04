01d8188c0de17921eb9207f6aa9d3dbfd551a9b9
Maciej Mądry <maciejmadry0@gmail.com>
2024-12-04 09:13:16 +0100

    ADD: response api interceptors to refresh expired access token

--- onyx-frontend/src/lib/hooks/useApiInterceptors.ts
+++ onyx-frontend/src/lib/hooks/useApiInterceptors.ts
@@ -1,13 +1,29 @@
-import { useEffect } from "react";
-import { AxiosInstance } from "axios";
+import { useLayoutEffect } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+
+import { refreshAccessToken } from "@/lib/api/user";
+import { useLongLivedTokenStore } from "@/store/auth/longLivedTokenStore";
+import { useAuthStore } from "@/store/auth/authStore";
+import { resetAllMemoryStores } from "@/store/initializeMemoryStore";
+import { resetAllPersistedStores } from "@/store/resetPersistedStores";
+
+import type {
+  AxiosInstance,
+  AxiosError,
+  InternalAxiosRequestConfig,
+} from "axios";
 
 export const useApiInterceptors = (
   api: AxiosInstance,
   accessToken: string | null,
 ) => {
-  useEffect(() => {
-    if (!accessToken) return;
+  const queryClient = useQueryClient();
+  const longLivedToken = useLongLivedTokenStore.use.longLivedToken();
+  const setLongLivedToken = useLongLivedTokenStore.use.setLongLivedToken();
+  const setAccessToken = useAuthStore.use.setAccessToken();
+  const setIsInitialized = useAuthStore.use.setIsInitialized();
 
+  useLayoutEffect(() => {
     const requestInterceptor = api.interceptors.request.use((config) => {
       if (accessToken) {
         config.headers.Authorization = `Bearer ${accessToken}`;
@@ -15,8 +31,44 @@ export const useApiInterceptors = (
       return config;
     });
 
+    const responseInterceptor = api.interceptors.response.use(
+      (response) => response,
+      async (error: AxiosError) => {
+        const config = error.config as InternalAxiosRequestConfig & {
+          _retry?: boolean;
+        };
+        if (error.response?.status === 403 && !config._retry) {
+          config._retry = true;
+
+          if (!longLivedToken) {
+            queryClient.clear();
+            resetAllMemoryStores();
+            resetAllPersistedStores();
+            setIsInitialized(true);
+            return;
+          }
+
+          try {
+            const {
+              accessToken: newAccessToken,
+              longLivedToken: newLongLivedToken,
+            } = await refreshAccessToken(longLivedToken);
+            setAccessToken(newAccessToken);
+            setLongLivedToken(newLongLivedToken);
+            config.headers.Authorization = `Bearer ${newAccessToken}`;
+            return api(config);
+          } catch (refreshError) {
+            console.error("Token refresh failed", refreshError);
+            throw refreshError;
+          }
+        }
+        return Promise.reject(error);
+      },
+    );
+
     return () => {
       api.interceptors.request.eject(requestInterceptor);
+      api.interceptors.response.eject(responseInterceptor);
     };
   }, [api, accessToken]);
 };
